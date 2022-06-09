04fe80eefdc235392e4f66c80a532ad05583c00c
Lee <kimaniyoung@gmail.com>
2022-06-09 16:17:16 -0400

    betty block me

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks if there is an uppercase character
- * 
- * Return: 1 means that c is uppercase, 0 it is not
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int letter)
 {
